346bcc593737a503a63fa585b007806985ecbf1a
Shankar <prajaktapanda3@gmail.com>
2026-06-11 10:15:30 +0530

    Switch SQLite to PostgreSQL

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = "library_secret_key_2026"
+    SECRET_KEY = os.getenv("SECRET_KEY", "library_secret_key_2026")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///library.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
